a39d38a169e9dffdd9675eb279d47d2381844b9f
goodshep <shepyoto@gmail.com>
2021-10-26 13:33:25 -0500

    initial

---
+++ exercise.js
@@ -0,0 +1,11 @@
+const firstName = "Joshua"
+let lastName = "Burns"
+var age = "29"
+
+let fullName = firstName + " " + lastName
+console.log(fullName)
+
+let city = "Nashville"
+let pastime = "guitar"
+let myStory = `My name is ${fullName}. I live in ${city}. My favorite pastime is ${pastime}.`;
+console.log(myStory)
