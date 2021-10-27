ee465f6facb771c520a01267f52dccf49c252296
goodshep <shepyoto@gmail.com>
2021-10-27 08:41:33 -0500

    added new assignment

---
+++ operators.js
@@ -0,0 +1,40 @@
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
+
+let c = 11;
+str = 11;
+str2 = "eleven";
+isPresent = true;
+firstName = "Jackie";
+lastName = "Chan";
+
+console.log(a + str);
+console.log(a + str2);
+console.log(a + isPresent);
+console.log(a + firstName);
+console.log(a + lastName);
+
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "williams";
+
+console.log(d == str3);
+console.log(d === str3);
+console.log(!isPresent2);
+console.log("eleven" == str4 && d >= str3);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
